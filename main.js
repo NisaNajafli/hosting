8eaf8c6d9a2ad42a31b1c42d924ff17bd60d749d
NisaNajafli <ncfliniss@gmail.com>
2025-11-12 16:13:38 +0400

    f

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ window.addEventListener("scroll", function () {
   }
 });
 document.addEventListener('DOMContentLoaded', function () {
-  new Swiper('.mySwiper', {
+  new Swiper('.swiperreviews', {
   loop: true, 
   slidesPerView: 1,
   spaceBetween: 30,
@@ -28,3 +28,19 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.swiperprice', {
+  loop: true, 
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: { delay: 3000, disableOnInteraction: false },
+  pagination: { el: '.swiper-pagination', clickable: true },
+  navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+  breakpoints: {
+    768: { slidesPerView: 1 },
+    1200: { slidesPerView: 4 }
+  }
+});
+});
+
+document.getElementById("year").textContent= new Date().getFullYear();
